b958404c8f54178126c6ae42efc1a6c0d9ea8909
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2020-03-28 00:28:49 +0000

    StartGame screen done

--- App.js
+++ App.js
@@ -8,66 +8,18 @@ import {
   Modal,
   Text
 } from "react-native";
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
+import { Header } from "react-native/Libraries/NewAppScreen";
 
 export default function App() {
-  const [goalList, setGoalList] = useState([]);
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const addItemHandler = enteredGoal => {
-    setGoalList(currentList => [
-      ...currentList,
-      { id: Math.random().toString(), text: enteredGoal }
-    ]);
-    setModalVisible(false);
-    Keyboard.dismiss();
-  };
-
-  const removeItemHandler = id => {
-    setGoalList(currentList => currentList.filter(item => item.id !== id));
-  };
-
-  const closeModal = () => {
-    setModalVisible(false);
-  };
   return (
-    <View style={styles.container}>
-      <Button title="Add New Goal" onPress={() => setModalVisible(true)} />
-
-      <GoalInput
-        addItemHandler={addItemHandler}
-        visible={modalVisible}
-        closeModal={closeModal}
-      />
-      <FlatList
-        style={styles.itemList}
-        data={goalList}
-        keyExtractor={itemData => itemData.id}
-        renderItem={({ item }) => {
-          //console.log(item);
-          return (
-            <GoalItem
-              id={item.id}
-              text={item.text}
-              removeItemHandler={removeItemHandler}
-            />
-          );
-        }}
-      />
+    <View style={styles.screen}>
+      <Header title="Guess numbers" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: 40,
-    flex: 1,
-    paddingHorizontal: 20,
-    justifyContent: "center"
-  },
-
-  itemList: {
-    marginTop: 10
+  screen: {
+    flex: 1
   }
 });
---
+++ components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+function Header({ title }) {
+  return (
+    <View style={styles.headerContainer}>
+      <Text style={styles.headerText}>{title}</Text>
+    </View>
+  );
+}
+
+export default Header;
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    width: "100%",
+    height: 90,
+    paddingTop: 40,
+    backgroundColor: "#ACF3D3",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  headerText: {
+    color: "#4b4b4c"
+  }
+});
